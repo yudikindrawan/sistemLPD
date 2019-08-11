1cb6447ff4f14bdff29b013a6a564c6dc4407fe3
Arti Bali Code <yudikidrr@gmail.com>
2019-08-12 00:09:04 +0800

    [UPDATE] Master 12/08

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,29 +5,29 @@
         <div class="container body">
         <div class="main_container">
             <div class="col-md-3 left_col">
-            <div class="left_col scroll-view">
-                <div class="navbar nav_title" style="border: 0;">
-                <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Sistem LPD!</span></a>
-                </div>
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title" style="border: 0;">
+                    <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Sistem LPD!</span></a>
+                    </div>
 
-                <div class="clearfix"></div>
+                    <div class="clearfix"></div>
 
-                <!-- menu profile quick info -->
-                <div class="profile clearfix">
-                <div class="profile_pic">
-                    <img src="{{ asset ('assets/production/images/img.jpg')}}" alt="..." class="img-circle profile_img">
-                </div>
-                <div class="profile_info">
-                    <span>Welcome,</span>
-                    <h2>John Doe</h2>
-                </div>
-                </div>
-                <!-- /menu profile quick info -->
+                    <!-- menu profile quick info -->
+                    <div class="profile clearfix">
+                    <div class="profile_pic">
+                        <img src="{{ asset ('assets/production/images/img.jpg')}}" alt="..." class="img-circle profile_img">
+                    </div>
+                    <div class="profile_info">
+                        <span>Welcome,</span>
+                        <h2>John Doe</h2>
+                    </div>
+                    </div>
+                    <!-- /menu profile quick info -->
 
-                <br />
+                    <br />
 
                 @include('layouts.sidebar')
-            </div>
+                </div>
             </div>
 
             <!-- top navigation -->
